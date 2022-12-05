23a0673c73b166a2237d693d54be602f4328650c
Helton Mori <heltonmori@gmail.com>
2022-12-04 22:53:26 -0300

    Header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,21 @@
 <template>
   <div id="app">
-
+    <HcodeHeader />
   </div>
 </template>
 
 <script>
 
+import HcodeHeader from './components/HcodeHeader'
+
 export default {
   name: 'App',
   components: {
+    HcodeHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url('https://fonts.googleapis.com/css?family=Rajdhani&display=swap');
 </style>
---
+++ src/components/HcodeHeader.vue
@@ -0,0 +1,52 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="navbar navbar-expand">
+                <a href="#" class="link navbar-brand mr-auto">
+                    <img src="./../assets/logo.svg" alt="Hcode treinamentos" class="img d-inline-block align-top" id="logo">Futebol clube
+                </a>
+                <div class="links mr-2">
+                    <a href="" class="btn mr-5">Início</a>
+                    <a href="" class="btn mr-5">Times</a>
+                    <a href="" class="btn mr-5">Classificações</a>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+header {
+    background-color: #000;
+}
+
+#logo {
+    width: 150px;
+    height: 100%;
+}
+
+.links {
+    display: inline-flex;
+}
+
+.links a {
+    color: #fff;
+}
+
+.links a:focus {
+    outline: none!important;
+    box-shadow: none!important;
+}
+
+.link {
+    color: #d48e23;
+    font-size: 30px;
+    font-family: 'Rajdhani';
+}
+</style>
