7b545ba292347bf2f8d84211b456508340e93039
Helton Mori <heltonmori@gmail.com>
2022-12-05 23:12:21 -0300

    Section news

---
+++ src/components/HcodeSectionNews.vue
@@ -0,0 +1,30 @@
+<template>
+    <section>
+        <div class="container">
+            <HcodeSectionNewsInvididual />
+            <HcodeSectionNewsInvididual />
+            <HcodeSectionNewsInvididual />
+        </div>
+    </section>
+</template>
+
+<script>
+import HcodeSectionNewsInvididual from './HcodeSectionNewsInvididual'
+
+export default {
+    components: {
+        HcodeSectionNewsInvididual
+    }
+}
+</script>
+
+<style scoped>
+
+section {
+    padding: 25px 0;
+    margin-top: 25px;
+    background-color: #f37520;
+}
+
+
+</style>
---
+++ src/components/HcodeSectionNewsInvididual.vue
@@ -0,0 +1,37 @@
+<template>
+    <section>
+        news ind
+        <div class="row">
+            <div class="col-3">
+                <img src="../assets/news1.jpg" alt="Noticia 1">
+            </div>
+            <div class="col-9">
+                <h2>Começam os treinos para a nova temporada</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus repudiandae delectus, praesentium quos ipsam minima, veritatis tempora rerum aliquam, quaerat aperiam unde. Quia quas repellat doloremque vel non amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt eaque ducimus ea accusantium perferendis nemo ad ab tempora doloribus omnis quos, cupiditate dolores deleniti saepe alias unde quisquam maiores.</p>
+
+                <span class="font-italic">01/01/2020</span>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.row {
+    margin-bottom: 30px;
+}
+
+img {
+    width: 100%;
+}
+
+h2 {
+    cursor: pointer;
+    color: #fff;
+}
+</style>
