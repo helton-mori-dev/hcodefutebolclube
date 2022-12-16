6fa18805c8c2732d1c1f13f5a4d9783476932e9c
Helton Mori <heltonmori@gmail.com>
2022-12-15 23:29:13 -0300

    V-model em componentes

--- src/components/HcodeSection.vue
+++ src/components/HcodeSection.vue
@@ -9,20 +9,38 @@
         <HcodeSectionBanner />
 
         <HcodeSectionNews />
+
+        <div class="container">
+            <div class="row my-club mt-5">
+                <div class="col-6">
+                    <h2>Seu clube é: {{ myClub }}</h2>
+                </div>
+                <div class="col-6">
+                    <HcodeInput v-model="myClub" />
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 
 <script>
 import HcodeSectionBanner from './HcodeSectionBanner'
 import HcodeSectionNews from './HcodeSectionNews'
+import HcodeInput from './HcodeInput'
 
 export default {
     components: {
         HcodeSectionBanner,
-        HcodeSectionNews
+        HcodeSectionNews,
+        HcodeInput
     },
     props: {
         championship: String
+    },
+    data () {
+        return {
+            myClub: 'Hcode treinamentos'
+        }
     }
 }
 </script>
