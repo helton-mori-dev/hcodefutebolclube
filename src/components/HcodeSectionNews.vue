ec91b8fb89ad9b78e58e12a00c5578bbf96c80d2
Helton Mori <heltonmori@gmail.com>
2023-09-25 23:27:01 -0300

    Mapgetters noticias

--- src/components/HcodeSectionNews.vue
+++ src/components/HcodeSectionNews.vue
@@ -1,15 +1,11 @@
 <template>
     <section>
         <div class="container">
-            <HcodeSectionNewsInvididual 
-                v-for="notice in news" 
-                :key=notice.id
-                :img-name="notice.img"
-                :img-info="notice.imgInfo"
-                :news-content="notice.content"
-                :news-date="notice.date"
-            >
-                <template #title><h2>{{ notice.title }}</h2></template>
+            <HcodeSectionNewsInvididual v-for="notice in news" :key=notice.id :img-name="notice.img"
+                :img-info="notice.imgInfo" :news-content="notice.content" :news-date="notice.date">
+                <template #title>
+                    <h2>{{ notice.title }}</h2>
+                </template>
                 <p>{{ notice.content | truncate(200) }}</p>
             </HcodeSectionNewsInvididual>
         </div>
@@ -18,51 +14,28 @@
 
 <script>
 import HcodeSectionNewsInvididual from './HcodeSectionNewsInvididual'
+import { mapGetters } from 'vuex';
 
 export default {
     components: {
         HcodeSectionNewsInvididual
     },
-    data () {
+    data() {
         return {
-            news: []
         }
     },
-    created () {
-        this.news = [{
-            id: 1,
-            title: 'Começam os treinos para a nova temporada'            ,
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus repudiandae delectus, praesentium quos ipsam minima, veritatis tempora rerum aliquam, quaerat aperiam unde. Quia quas repellat doloremque vel non amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt eaque ducimus ea accusantium perferendis nemo ad ab tempora doloribus omnis quos, cupiditate dolores deleniti saepe alias unde quisquam maiores.',
-            date: '2020-01-01',
-            img: 'news2.jpg',
-            imgInfo: 'Notícia 2'
-        },{
-            id: 2,
-            title: 'Jogo de quarta termina empatado'            ,
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus repudiandae delectus, praesentium quos ipsam minima, veritatis tempora rerum aliquam, quaerat aperiam unde. Quia quas repellat doloremque vel non amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt eaque ducimus ea accusantium perferendis nemo ad ab tempora doloribus omnis quos, cupiditate dolores deleniti saepe alias unde quisquam maiores.',
-            date: '2020-01-07',
-            img: 'news2.jpg',
-            imgInfo: 'Notícia 2'
-        },{
-            id: 3,
-            title: 'A inauguração do estádio será semana que vem'            ,
-            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus repudiandae delectus, praesentium quos ipsam minima, veritatis tempora rerum aliquam, quaerat aperiam unde. Quia quas repellat doloremque vel non amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt eaque ducimus ea accusantium perferendis nemo ad ab tempora doloribus omnis quos, cupiditate dolores deleniti saepe alias unde quisquam maiores.',
-            date: '2020-01-20',
-            img: 'news3.jpg',
-            imgInfo: 'Notícia 3'
-        }
-    ]
+    computed: {
+        ...mapGetters({
+            news: 'getNews'
+        })
     }
 }
 </script>
 
 <style scoped>
-
 section {
     padding: 25px 0;
     margin-top: 25px;
     background-color: #f37520;
 }
-
-
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,30 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
         championship: 'Campeonato Brasileirão',
-        clubName: 'Hcode treinamentos 10'
+        clubName: 'Hcode treinamentos 10',
+        news: [{
+            id: 1,
+            title: 'Começam os treinos para a nova temporada',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus repudiandae delectus, praesentium quos ipsam minima, veritatis tempora rerum aliquam, quaerat aperiam unde. Quia quas repellat doloremque vel non amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt eaque ducimus ea accusantium perferendis nemo ad ab tempora doloribus omnis quos, cupiditate dolores deleniti saepe alias unde quisquam maiores.',
+            date: '2020-01-01',
+            img: 'news2.jpg',
+            imgInfo: 'Notícia 2'
+        }, {
+            id: 2,
+            title: 'Jogo de quarta termina empatado',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus repudiandae delectus, praesentium quos ipsam minima, veritatis tempora rerum aliquam, quaerat aperiam unde. Quia quas repellat doloremque vel non amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt eaque ducimus ea accusantium perferendis nemo ad ab tempora doloribus omnis quos, cupiditate dolores deleniti saepe alias unde quisquam maiores.',
+            date: '2020-01-07',
+            img: 'news2.jpg',
+            imgInfo: 'Notícia 2'
+        }, {
+            id: 3,
+            title: 'A inauguração do estádio será semana que vem',
+            content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas necessitatibus repudiandae delectus, praesentium quos ipsam minima, veritatis tempora rerum aliquam, quaerat aperiam unde. Quia quas repellat doloremque vel non amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere sunt eaque ducimus ea accusantium perferendis nemo ad ab tempora doloribus omnis quos, cupiditate dolores deleniti saepe alias unde quisquam maiores.',
+            date: '2020-01-20',
+            img: 'news3.jpg',
+            imgInfo: 'Notícia 3'
+        }
+        ],
     },
     getters: {
         getChampionship(state) {
@@ -14,6 +37,9 @@ export default new Vuex.Store({
         },
         getClubName(state) {
             return state.clubName
+        },
+        getNews(state) {
+            return state.news
         }
     }
 })
